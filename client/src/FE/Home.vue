17fe69d758be57d9f58d7917c59d645dc80bc582
Pikago <wuxiaozhu9821@hotmail.com>
2022-11-07 05:05:49 -0600

    Homepage

--- client/src/FE/Home.vue
+++ client/src/FE/Home.vue
@@ -1,13 +1,242 @@
 <template>
-  <v-layout>
-    <v-app-bar height="22" color="grey-darken-3" elevation="0"></v-app-bar>
-    <v-navigation-drawer width="72" color="grey-darken-2" permanent></v-navigation-drawer>
-    <v-navigation-drawer width="150" color="grey-darken-1" permanent></v-navigation-drawer>
-    <v-app-bar height="48" color="grey" elevation="0"></v-app-bar>
-    <v-navigation-drawer location="right" width="150" color="grey-lighten-1" permanent></v-navigation-drawer>
-    <v-app-bar location="bottom" height="48" color="grey-lighten-2" elevation="0"></v-app-bar>
+  <v-app>
+    <div>
+      <v-app-bar
+        color="blue accent-4"
+        dense
+        dark
+      >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>BMWPHD</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu leftbottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+          <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+      </v-menu>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Manage</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-file-document</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>History</v-list-item-title>
+            </v-list-item>
+
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
+
     <v-main>
-      <v-card elevation="0" height="700px"></v-card>
-    </v-main>
-  </v-layout>
+      <v-card
+        class="mx-auto my-16"
+        flat
+        width="800px"
+        height="80px"
+      >
+    <v-toolbar
+      dense
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>mdi-microphone</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-format-list-checkbox</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+
+  <div id="app">
+      <v-container>
+        <v-select v-model="selectedCategories" :items="categories" attach label="Search by Categories" multiple>
+          <template v-slot:prepend-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-text-field v-model="searchTerm" placeholder="Search" @input="searchCategories"></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider class="mt-2"></v-divider>
+          </template>
+        </v-select>
+      </v-container>
+  </div>
+
+        <v-sheet class="py-16">
+          <section id="filter">
+            <v-container>
+              <v-row justify="space-between">
+                <v-col cols="auto">
+                  <v-responsive width="350">
+                    <h2 class="text-h4">
+                      BMW Performance Horse Database
+                    </h2>
+
+                    <p class="text-success mt-3">
+                      (940) 357-1998
+                    </p>
+
+                    <p class="mt-8">
+                      BMW Quarter Horses breeds and raises prospects for the reining and reined cowhorse disciplines with a focus on pedigree, conformation, a good mind, and soundness. 
+                    </p>
+
+                    <p class="mt-8">
+                      We believe in mare power and always have a range of prospects available
+                    </p>
+
+                    <v-btn
+                      class="mt-6"
+                      href="https://www.facebook.com/BMWqh/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Facebook Homepage
+                    </v-btn>
+                  </v-responsive>
+                </v-col>
+
+                <v-img
+                  max-width="600"
+                  src="../client/src/assets/BMWPHD.png"
+                />
+              </v-row>
+            </v-container>
+          </section>
+        </v-sheet>
+      </v-main>
+
+      <v-footer class="bg-grey-lighten-1">
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          <strong>{{ new Date().getFullYear() }} — BMWPHD</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+  export default { name: 'App',
+  data: () => ({
+    drawer:false,
+    group:null,
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
+
+    searchTerm: "",
+    categories: [
+      "Name",
+      "sire",
+      "dam",
+      "dam sire",
+      "2nd dam",
+      "lte",
+      "pe",
+      "show",
+      "class",
+      "level",
+      "open vs non pro",
+      "age",
+      "place",
+      "money",
+      "breeder",
+      "owner",
+      "rider",
+      "draw",
+      "back number",
+      "on dirt",
+      "finalist",
+      "maneuver scores",
+      "notes",
+      "nrha",
+      "date of show/class",
+      "schooling"
+    ],
+    categoriesCopy: [],
+    selectedCategories: []
+  }),
+
+  mounted() {
+    this.categoriesCopy = [...this.categories];
+  },
+
+  computed: {},
+
+  methods: {
+    searchCategories(e) {
+      if (!this.searchTerm) {
+        this.categories = this.categoriesCopy;
+      }
+
+      this.categories = this.categoriesCopy.filter((category) => {
+        return category.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+      });
+    }
+  }
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,6 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
-
-// Plugins
-import { registerPlugins } from '../src/plugins/index.js'
+import { registerPlugins } from './plugins/index.js'
 import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
