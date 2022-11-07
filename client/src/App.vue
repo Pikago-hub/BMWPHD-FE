73d5b65dcad4e975a999ef96a7ef5d5920d61e98
Pikago <wuxiaozhu9821@hotmail.com>
2022-11-06 23:49:47 -0600

    homepage update

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+      <v-main>
+        <Home />
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+    import Home from '../src/FE/Home.vue'
+  </script>
+  
