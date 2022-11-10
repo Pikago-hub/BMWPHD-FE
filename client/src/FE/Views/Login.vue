560daa30bbb11b0c9c12823f4918a98cee17a550
madison-gresham <99980236+madison-gresham@users.noreply.github.com>
2022-11-10 14:54:29 -0600

    updated login to vuetify

--- client/src/FE/Views/Login.vue
+++ client/src/FE/Views/Login.vue
@@ -1,5 +1,6 @@
 <template>
-   <v-app >
+   <!-- <v-app >
+
       <v-main>
          <v-container fluid fill-height>
                <v-flex xs12 sm8 md4>
@@ -33,7 +34,78 @@
                </v-flex>
          </v-container>
       </v-main>
-   </v-app>
+   </v-app> -->
+   <v-app>
+   <div class="py-4">
+      <!-- <v-img
+        class="mx-auto mb-10"
+        max-width="228"
+        src="../client/src/assets/BMWPHD.png"
+      ></v-img> -->
+  
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+  
+        <div class="text-subtitle-1 text-medium-emphasis">Username</div>
+  
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+  
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+  
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a>
+        </div>
+  
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+  
+  
+        <v-btn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+          @click="onSubmit"
+        >
+          Log In
+        </v-btn>
+  
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </div>
+  </v-app>
 </template>
 
 <script>
