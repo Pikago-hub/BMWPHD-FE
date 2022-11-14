38d09cb09a4613aa3fc0afbda8ecdd7bebf659e5
Zhaoyu Wu <jerry.w@Pikago-MBP.local>
2022-11-14 11:40:24 -0600

    tiny change

--- client/src/FE/Views/Info.vue
+++ client/src/FE/Views/Info.vue
@@ -115,3 +115,84 @@
       </v-footer>
     </v-app>
   </template>
+    
+  <script>
+      import {useRouter} from 'vue-router'
+      const router = useRouter()
+    
+    
+      export default { 
+        name: 'App',
+        data: () => ({
+          drawer: false,
+          group: null,
+          searchTerm: "",
+          categories: [
+            "Name",
+            "sire",
+            "dam",
+            "dam sire",
+            "2nd dam",
+            "lte",
+            "pe",
+            "show",
+            "class",
+            "level",
+            "open vs non pro",
+            "age",
+            "place",
+            "money",
+            "breeder",
+            "owner",
+            "rider",
+            "draw",
+            "back number",
+            "on dirt",
+            "finalist",
+            "maneuver scores",
+            "notes",
+            "nrha",
+            "date of show/class",
+            "schooling"
+          ],
+          categoriesCopy: [],
+          selectedCategories: []
+        }),
+    
+      mounted() {
+        this.categoriesCopy = [...this.categories];
+      },
+    
+      computed: {},
+    
+      methods: {
+        searchCategories(e) {
+          if (!this.searchTerm) {
+            this.categories = this.categoriesCopy;
+          }
+    
+          this.categories = this.categoriesCopy.filter((category) => {
+            return category.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+          });
+        },
+        onSearch(){
+            this.$router.push('/result');
+        }, 
+        onHome(){
+            this.$router.push('/home');
+        }, 
+        onManage(){
+            this.$router.push('/home');
+        }, 
+        onHistory(){
+            this.$router.push('/history');
+        }, 
+        onLogin(){
+            this.$router.push('/login');
+        }, 
+        onAbout(){
+            this.$router.push('/about');
+        }, 
+      }
+    }
+    </script>
