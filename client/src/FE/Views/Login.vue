93d85597a7ca929e1532e64fd4925faad72e4b10
madison-gresham <99980236+madison-gresham@users.noreply.github.com>
2023-01-26 20:13:39 -0600

    signup and login validation

--- client/src/FE/Views/Login.vue
+++ client/src/FE/Views/Login.vue
@@ -21,112 +21,129 @@
       <v-card-text>
         <v-window v-model="tab">
           <v-window-item value="Login">
-              <div class="text-subtitle-1 text-medium-emphasis">Username</div>
-              <v-text-field
-                density="compact"
-                placeholder="Email address"
-                prepend-inner-icon="mdi-email-outline"
-                variant="outlined"
-              ></v-text-field>
-        
-              <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Password
-                <a
-                  class="text-caption text-decoration-none text-blue"
-                  href="/PageNotFound"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Forgot login password?</a>
-              </div>
-        
-              <v-text-field
-                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="visible ? 'text' : 'password'"
-                density="compact"
-                placeholder="Enter your password"
-                prepend-inner-icon="mdi-lock-outline"
-                variant="outlined"
-                @click:append-inner="visible = !visible"
-              ></v-text-field>
-        
-              <v-btn
-                block
-                class="mb-8"
-                color="blue"
-                size="large"
-                variant="tonal"
-                @click="onSubmit"
-              >
-                Log In
-              </v-btn>
+            <v-card class="px-4">
+            <v-card-text>
+              <v-form ref="loginForm" v-model="valid" lazy-validation>
 
+                    <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+                    <v-text-field 
+                      density="compact" 
+                      placeholder="Email address" 
+                      prepend-inner-icon="mdi-email-outline"
+                      variant="outlined"
+                      v-model="loginEmail" 
+                      :rules="loginEmailRules" 
+                      required>
+                    </v-text-field>
+    
+                    <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"> Password </div>
+                    <v-text-field 
+                      density="compact"
+                      placeholder="Enter your password"
+                      prepend-inner-icon="mdi-lock-outline"
+                      variant="outlined"
+                      v-model="loginPassword" 
+                      :rules="[rules.required, rules.min]" 
+                      label="Password" 
+                      hint="At least 8 characters" 
+                      counter 
+                      @click:append="show1 = !show1">
+                    </v-text-field>
 
+                    <v-btn 
+                    x-large block 
+                    :disabled="!valid" 
+                    color="blue"
+                    class="mb-8 mt-3" 
+                    variant="tonal"
+                    @click="validate"> Login </v-btn>
+
+              </v-form>
+            </v-card-text>
+            </v-card>
           </v-window-item>
   
-          <v-window-item value="Sign up">
+          <v-window-item value="Sign up"><v-card class="px-4">
+             <v-card-text>
+              <v-form ref="registerForm" v-model="valid" lazy-validation>
               <v-row>
-                <v-col cols="12" sm="6" md="6">
-                    <div class="text-subtitle-1 text-medium-emphasis">First Name</div>
-                    <v-text-field
-                      density="compact"
-                      placeholder="First Name"
-                      prepend-inner-icon="mdi-account-box"
-                      variant="outlined"
-                    ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="6">
-                  <div class="text-subtitle-1 text-medium-emphasis">Last Name</div>
-                  <v-text-field
-                    density="compact"
-                    placeholder="Last Name"
-                    prepend-inner-icon="mdi-account-box"
-                    variant="outlined"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <div class="text-subtitle-1 text-medium-emphasis">Email Address</div>
-              <v-text-field
-                density="compact"
-                placeholder="Email Address"
-                prepend-inner-icon="mdi-email-outline"
-                variant="outlined"
-              ></v-text-field>
-        
-              <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Password
-              </div>
-              <v-text-field
-         :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-         :type="visible ? 'text' : 'password'"
-         density="compact"
-         placeholder="Enter your password"
-         prepend-inner-icon="mdi-lock-outline"
-         variant="outlined"
-         @click:append-inner="visible = !visible"
-       ></v-text-field>
-       <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        Confirm Password
-       </div>
-       <v-text-field
-         :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-         :type="visible ? 'text' : 'password'"
-         density="compact"
-         placeholder="Enter your password again"
-         prepend-inner-icon="mdi-lock-outline"
-         variant="outlined"
-         @click:append-inner="visible = !visible"
-       ></v-text-field>
-       <v-btn
-         block
-         class="mb-8"
-         color="blue"
-         size="large"
-         variant="tonal"
-         @click="onSubmit"
-       >
-         Sign Up
-       </v-btn>
+              <v-col cols="12" sm="6" md="6">
+                <div class="text-subtitle-1 text-medium-emphasis">First Name</div>
+                <v-text-field 
+                  density="compact"
+                  placeholder="First Name"
+                  prepend-inner-icon="mdi-account-box"
+                  variant="outlined"
+                  v-model="firstName" 
+                  :rules="[rules.required]" 
+                  maxlength="20" 
+                  required>
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <div class="text-subtitle-1 text-medium-emphasis">Last Name</div>
+                <v-text-field 
+                  density="compact"
+                  placeholder="Last Name"
+                  prepend-inner-icon="mdi-account-box"
+                  variant="outlined"
+                  v-model="lastName" 
+                  :rules="[rules.required]" 
+                  maxlength="20" 
+                  required>
+                </v-text-field>
+              </v-col>
+            </v-row>
+                <div class="text-subtitle-1 text-medium-emphasis">Email Address</div>
+                <v-text-field 
+                  density="compact"
+                  placeholder="Email Address"
+                  prepend-inner-icon="mdi-email-outline"
+                  variant="outlined"
+                  v-model="email" 
+                  :rules="emailRules" 
+                  required>
+                </v-text-field>
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">Password</div>
+                <v-text-field 
+                  density="compact"
+                  placeholder="Enter your password"
+                  prepend-inner-icon="mdi-lock-outline"
+                  variant="outlined"
+                  v-model="password" 
+                  :rules="[rules.required, rules.min]" 
+                  name="input-10-1"  
+                  hint="At least 8 characters" 
+                  counter 
+                  @click:append="show1 = !show1">
+                </v-text-field>
+
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">Confirm Password</div>
+                <v-text-field 
+                  block 
+                  density="compact"
+                  placeholder="Enter your password again"
+                  prepend-inner-icon="mdi-lock-outline"
+                  variant="outlined"
+                  v-model="verify" 
+                  :rules="[rules.required, passwordMatch]" 
+                  name="input-10-1" 
+                  counter 
+                  @click:append="show1 = !show1">
+                </v-text-field>
+          
+                <v-btn 
+                  x-large 
+                  block :disabled="!valid" 
+                  color="blue"
+                  variant="tonal"
+                  class="mb-8"
+                  @click="validate">Register
+                </v-btn>
+             
+              </v-form>
+              </v-card-text>
+              </v-card>
           </v-window-item>
         
   
@@ -141,25 +158,54 @@
 </template>
 
 <script>
-import {useRouter} from 'vue-router'
-const router = useRouter()
-
 export default {
-  name: "Login",
-  data() {
-    return {
-      username: "",
-      password: "",
-      tab: null,
-    };
+  data: () => ({
+    dialog: true,
+    tab: 0,
+    tabs: [
+        {name:"Login", icon:"mdi-account"},
+        {name:"Register", icon:"mdi-account-outline"}
+    ],
+    valid: true,
+    
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    verify: "",
+    loginPassword: "",
+    loginEmail: "",
+    loginEmailRules: [
+      v => !!v || "Required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    emailRules: [
+      v => !!v || "Required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+
+    show1: false,
+    rules: {
+      required: value => !!value || "Required.",
+      min: v => (v && v.length >= 8) || "Min 8 characters"
+    }
+  }),
+  computed: {
+    passwordMatch() {
+      return () => this.password === this.verify || "Password must match";
+    }
   },
   methods: {
-    login() {
-      const { username } = this;
-      console.log(username + "logged in")
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        this.$router.push('/');
+      }
     },
-    onSubmit() {
-      this.$router.push('/');
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
     }
   },
 };
