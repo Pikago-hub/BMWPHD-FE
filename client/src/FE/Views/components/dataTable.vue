74a194b99e1a87eab753f9091b763a2a5527733d
Pikago <wuxiaozhu9821@hotmail.com>
2023-02-06 08:05:02 -0600

    table

---
+++ client/src/FE/Views/components/dataTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <EasyDataTable
+    id="table"
+    :headers="headers"
+    :items="items"
+    :rows-per-page="25"
+  >
+    <searchTool @horseSearch="getHorses" />
+    <template #pagination="{ prevPage, nextPage, isFirstPage, isLastPage }">
+      <button :disabled="isFirstPage" @click="prevPage">prev page</button>
+      <button :disabled="isLastPage" @click="nextPage">next page</button>
+    </template>
+  </EasyDataTable>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Header } from "vue3-easy-data-table";
+import searchTool from "./searchTool.vue";
+
+export default defineComponent({
+  name: "DataTable",
+  components: {
+    searchTool,
+  },
+
+  methods: {
+    getHorses(horseSearch) {
+      console.log(horseSearch);
+    },
+  },
+
+  setup() {
+    const headers: Header[] = [
+      { text: "Name", value: "name" },
+      { text: "Sire", value: "sire1" },
+      { text: "Dam", value: "dam1" },
+      { text: "Dam Sire", value: "sire2" },
+      { text: "2nd Dam", value: "dam2" },
+    ];
+    function getHorses(horseSearch) {
+      console.log(horseSearch);
+    }
+
+    const items = getHorses(34110);
+    return {
+      getHorses,
+      headers,
+      items,
+    };
+  },
+});
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,25 +1,21 @@
-import App from './App.vue'
-import { createApp } from 'vue'
-import { registerPlugins } from './plugins/index.js'
-import vuetify from './plugins/vuetify'
-import router from'./router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import { VueClipboard } from '@soerenmartius/vue3-clipboard'
+import App from "./App.vue";
+import { createApp } from "vue";
+import { registerPlugins } from "./plugins/index.js";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { VueClipboard } from "@soerenmartius/vue3-clipboard";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
+const app = createApp(App);
 
-const app = createApp(App)
-
-
-
-registerPlugins(app)
+registerPlugins(app);
 app
   .use(VueAxios, axios)
   .use(router)
   .use(vuetify)
+  .component("EasyDataTable", Vue3EasyDataTable)
   .use(VueClipboard)
-  .mount('#app')
-
-
-
-
+  .mount("#app");
