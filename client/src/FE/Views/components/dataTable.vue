acf747e854b69e4deb126c875220adce093fb299
Pikago-hub <wuxiaozhu9821@hotmail.com>
2023-02-21 15:38:48 -0600

    form for horses details template without data populated, I have to somewhere

--- client/src/FE/Views/components/dataTable.vue
+++ client/src/FE/Views/components/dataTable.vue
@@ -9,16 +9,108 @@
     :items="items"
     buttons-pagination
   >
+    <template #item-operation="item">
+      <v-row justify="center">
+        <v-dialog v-model="dialog" persistent width="1024">
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" v-bind="props"> Details </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Horse Detailed Information</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Dam"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Sire"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Dam Sire"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="2nd Dam"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="European OPT"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Foal Date"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Horse Class"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Level"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="LTE"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Maneuver Score"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Nominator"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="PE"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Show"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field label="Year"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="dialog = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="dialog = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </template>
   </EasyDataTable>
 </template>
 
 <script>
 import { defineComponent, ref } from "vue";
+import axios from "axios";
+
 export default defineComponent({
   name: "DataTable",
   props: ["dataHorses"],
   data: () => {
     return {
+      dialog: false,
+      showModal: false,
+      formTitle: "Horse Details",
+      formData: {
+        name: "1",
+        sire: "1",
+        // Add more fields for each field in your form
+      },
       headers: [
         { text: "Name", value: "name" },
         { text: "Sire", value: "sire1" },
@@ -29,6 +121,7 @@ export default defineComponent({
         { text: "European OPT", value: "european_opt" },
         { text: "Year", value: "year", sortable: true },
         { text: "Nominator", value: "nominator" },
+        { text: "Operation", value: "operation", sortable: false },
       ],
       items: [],
       itemsSelected: ref([]),
@@ -50,6 +143,11 @@ export default defineComponent({
 
     assignData() {
       this.items = this.dataHorses;
+    },
+
+    cancel() {
+      this.showModal = false;
+      // Reset the form data here
     },
   },
 });
